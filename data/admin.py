ba80f1f9edce26d090ab0d1502741aeed91d9926
juan <j.m.bonomo1982@gmail.com>
2019-04-03 03:09:07 -0300

    Agregado de modelos

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Prov
+from .models import Prov, Fabricante, Sector, tipo_Producto, Producto, Lote_producto
 # Register your models here.
 
 admin.site.register(Prov)
+admin.site.register(Fabricante)
+admin.site.register(Sector)
+admin.site.register(tipo_Producto)
+admin.site.register(Producto)
+admin.site.register(Lote_producto)
--- data/models.py
+++ data/models.py
@@ -10,3 +10,55 @@ class Prov(models.Model):
     def __str__(self):
         return self.nombre
 
+class Fabricante(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length = 200)
+    descripcion = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Sector(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    codigo = models.CharField(max_length = 10)
+    descripcion = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.codigo
+
+
+class tipo_Producto(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    codigo = models.CharField(max_length = 10)
+    descripcion = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.codigo
+
+
+
+
+class Producto(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    codigo = models.CharField(max_length = 100)
+    descripcion = models.CharField(max_length = 200)
+    tipo = models.ForeignKey(tipo_Producto, on_delete=models.CASCADE)
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    observacion = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        r = self.descripcion
+        return r
+
+class Lote_producto(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    codigo = models.CharField(max_length = 100)
+    fecha_venc = models.DateField(auto_now=False)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, default=None)
+    observacion = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        r = self.codigo + " de " + self.producto
+        return r
+
