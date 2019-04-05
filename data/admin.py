c7e8c1e3140efcf203658f7fade439ca88b7b98e
juan <j.m.bonomo1982@gmail.com>
2019-04-05 02:08:19 -0300

    Agregado de modelos

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Prov, Fabricante, Sector, tipo_Producto, Producto, Lote_producto
+from .models import Prov, Fabricante, Sector, tipo_Producto, Producto, Lote_producto, Remito, Consumo_sector,Salida,Prestamo_e,Prestamo_s, Entrada
 # Register your models here.
 
 admin.site.register(Prov)
@@ -8,3 +8,9 @@ admin.site.register(Sector)
 admin.site.register(tipo_Producto)
 admin.site.register(Producto)
 admin.site.register(Lote_producto)
+admin.site.register(Remito)
+admin.site.register(Entrada)
+admin.site.register(Salida)
+admin.site.register(Consumo_sector)
+admin.site.register(Prestamo_e)
+admin.site.register(Prestamo_s)
--- data/models.py
+++ data/models.py
@@ -45,6 +45,7 @@ class Producto(models.Model):
     descripcion = models.CharField(max_length = 200)
     tipo = models.ForeignKey(tipo_Producto, on_delete=models.CASCADE)
     fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    sector = models.CharField(max_length = 200,default=None, help_text="Especificar que sector se encarga de pedir este insumo.")
     observacion = models.CharField(max_length = 200)
     
     def __str__(self):
@@ -61,4 +62,105 @@ class Lote_producto(models.Model):
     def __str__(self):
         r = self.codigo + " de " + self.producto
         return r
+
+
+class Remito(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nro_remito = models.CharField(max_length = 100)
+    fecha = models.DateField(auto_now=False)
+    observaciones = models.CharField(max_length = 200)
+
+    def __str__(self):
+        r = self.nro_remito
+        return r
+
+
+
+class Entrada(models.Model):
+    TIPO =(
+        ('PROV','Proveedor'),
+        ('PRES', 'Prestamo'),
+        ('DEVO','Dev Prestamo')
+    )
+
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    tipo = models.CharField(max_length = 4,choices=TIPO, default='PROV')
+    remito = models.ForeignKey(Remito, on_delete=models.CASCADE,default=None, help_text="Dejar vacío si el artículo es la devolución de préstamo, o préstamo")
+    articulo = models.ForeignKey(Lote_producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        r = "Item " + self.articulo + " de "  + self.remito
+        return r
+
+class Consumo_sector(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    fecha = fecha_ingreso = models.DateTimeField(default= timezone.now)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    observaciones = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        r = str(self.fecha)
+        s = self.sector
+        return r + s 
+
+
+
+class Salida(models.Model):
+    TIPO =(
+        ('PROV','Proveedor'),
+        ('PRES', 'Prestamo'),
+        ('DEVO','Dev Prestamo')
+    )
+    tipo = models.CharField(max_length = 4,choices=TIPO,default='PROV')
+    consumo_sector = models.ForeignKey(Consumo_sector, on_delete=models.CASCADE, default=None, help_text="Dejar vacio si se trata de un préstamo.")
+    articulo = models.ForeignKey(Lote_producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        r = "Item " + self.articulo + " de "  + self.consumo_sector
+        return r
+
+class Laboratorio(models.Model):
+    ingresado = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length = 200)
+    fecha_ingreso = models.DateTimeField(default= timezone.now)
+
+    def __str__(self):
+        return self.nombre
+
+    
+class Prestamo_s(models.Model):
+    #Para contabilizar los prestamos que hacemos o que devolvemos
+    TIPO =(
+        ('DEVS', 'Devolvemos')
+        ('PRES', 'Prestamos'),
+    )
+    tipo = models.CharField(max_length = 4,choices=TIPO)
+    fecha_prestamo = models.DateField(auto_now=False)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    destino = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    saliente = models.ForeignKey(Salida, on_delete=models.CASCADE)
+
+    def __str__(self):
+        r = self.tipo + " " + self.destino + " de "  + self.sector
+        return r
+
+
+class Prestamo_e(models.Model):
+    #Para contabilizar los prestamos que nos hacen o devuelven.
+    TIPO =(
+        ('DEVN', 'Devuelven')
+        ('PREN', 'Prestan'),
+    )
+    tipo = models.CharField(max_length = 4,choices=TIPO)
+    fecha_prestamo = models.DateField(auto_now=False)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    destino = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    saliente = models.ForeignKey(Salida, on_delete=models.CASCADE)
+
+    def __str__(self):
+        r = self.tipo + " " + self.destino + " de "  + self.sector
+        return r
+
 
